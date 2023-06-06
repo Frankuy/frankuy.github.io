9076b9f55ebcb66cfba48e434bda0f3cdd2a0ae5
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2023-06-06 12:29:46 +0700

    Format code

--- sw.js
+++ sw.js
@@ -1,32 +1,30 @@
-const cacheName = "cache-v1"
+const cacheName = "cache-v1";
 const resourcesToPrecache = [
-    '/',
-    'index.html',
-    'styles/main.css',
-    'scripts/main.js',
-    'images/icon.svg',
-]
+  "/",
+  "index.html",
+  "styles/main.css",
+  "scripts/main.js",
+  "images/icon.svg",
+];
 
+self.addEventListener("install", (event) => {
+  console.log("Install event");
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(resourcesToPrecache);
+    })
+  );
+});
 
-self.addEventListener('install', event => {
-    console.log("Install event")
-    event.waitUntil(
-        caches.open(cacheName)
-            .then(cache => {
-                return cache.addAll(resourcesToPrecache)
-            })
-    )
-})
+self.addEventListener("activate", (event) => {
+  console.log("Activate event");
+});
 
-self.addEventListener('activate', event => {
-    console.log('Activate event')
-})
-
-self.addEventListener('fetch', event => {
-    console.log("Fetch event")
-    event.respondWith(caches.match(event.request)
-        .then(cachedResponse => {
-            return cachedResponse || fetch(event.request)
-        })
-    )
-})
+self.addEventListener("fetch", (event) => {
+  console.log("Fetch event");
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
