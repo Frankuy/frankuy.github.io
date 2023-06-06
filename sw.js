c15cd39d27d58666ce971d2d840f0ff40a49acfe
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2023-06-06 09:47:46 +0700

    Restructure and update sw

---
+++ sw.js
@@ -0,0 +1,31 @@
+const cacheName = "cache-v1"
+const resourcesToPrecache = [
+    '/',
+    'index.html',
+    'styles/main.css',
+    'images/icon.svg'
+]
+
+
+self.addEventListener('install', event => {
+    console.log("Install event")
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache)
+            })
+    )
+})
+
+self.addEventListener('activate', event => {
+    console.log('Activate event')
+})
+
+self.addEventListener('fetch', event => {
+    console.log("Fetch event")
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            return cachedResponse || fetch(event.request)
+        })
+    )
+})
