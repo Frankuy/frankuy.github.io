1f1be8f1ddb61ab0ce068e0aac9dd92a9130425e
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2023-06-07 17:18:41 +0700

    Change css loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const { GenerateSW } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env, argv) => ({
   entry: "./src/index.js",
@@ -11,14 +12,19 @@ module.exports = (env, argv) => ({
     static: false,
   },
   output: {
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
-    rules: [{ test: /\.css$/i, use: ["style-loader", "css-loader"] }],
+    rules: [
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+    ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
