3824fea68c490d148be8d1c3f22fec7c32d07801
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2023-06-07 10:46:38 +0700

    Add webpack as bundler

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [{ test: /\.css$/i, use: ["style-loader", "css-loader"] }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "" }],
+    }),
+    new GenerateSW({}),
+  ],
+};
