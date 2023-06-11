872a4cdd7a84bd7995239c032686eb7e54351f07
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2023-06-11 11:40:46 +0700

    Add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,66 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Muhammad Fikri Hizbullah",
+        short_name: "MFH",
+        description: "Muhammad Fikri Hizbullah Portofolio",
+        scope: "/",
+        start_url: "/",
+        background_color: "#2C5364",
+        theme_color: "#2C5364",
+        display: "standalone",
+        icons: [
+          {
+            src: "/images/48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "/images/72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/images/96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/images/128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/images/192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/images/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/images/640x640.png",
+            sizes: "640x640",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
